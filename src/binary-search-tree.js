f512774c86fb8722b248ca72a1f9624d2fa05596
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-05-02 14:37:49 +0300

    + binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,8 @@
+// npm test -- test/binary-search-tree.test
+
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,40 +10,147 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.r = null;
+	}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	// ^------------------------ root ------------------------
+	root() {
+		return this.r;
+	}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	// ^------------------------ add ------------------------
+	add(data) {
+		this.r = putNode(this.r, data);
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		function putNode(node, data) {
+			if (node === null) return new Node(data); // if node is null, return new node
+			if (node.data === data) return node; // if node data is equal to data, return node
+
+			// if put data is less thaan current node data try to put new node
+			if (data < node.data) {
+				node.left = putNode(node.left, data);
+			} else if (data > node.data){
+				node.right = putNode(node.right, data);
+			}
+
+			return node;
+		}
+
+
+	}
+
+
+	// ^------------------------ has ------------------------
+	has(data) {
+
+		function hasNode(node, data) {
+			if (node === null) { return false; } // if no node return false that meeans cant find data
+			if (node.data === data) return node; // if node data is equal to data, return node
+
+			// if put data is try to find in matching node
+			if (data < node.data) {
+				return hasNode(node.left, data);
+			} else if (data > node.data) {
+				return hasNode(node.right, data);
+			}
+		}
+
+		return hasNode(this.r, data) ? true : false;
+	}
+
+
+	// ^------------------------ find ------------------------
+	find(data) {
+
+		function findNode(node, data) {
+
+			if (node === null) return null;
+
+			if (node.data === data) return node;
+
+			if (data < node.data) {
+				return findNode(node.left, data);
+			} else if (data > node.data) {
+				return findNode(node.right, data);
+			}
+
+		}
+
+		return findNode(this.r, data);
+	}
+
+
+	// ^------------------------ remove ------------------------
+	remove(data) {
+		this.r = removeNode(this.r, data);
+
+		function removeNode(node, data) {
+			if (node === null) return null; // if cant find node return null
+			// if (data === node.data) return null;
+
+			if (data < node.data) {
+				node.left = removeNode(node.left, data); // update subTree
+				return node; // update node
+			} else if (data > node.data) {
+				node.right = removeNode(node.right, data);
+				return node;
+			} else {
+				if (node.left === null && node.right === null) { // if node is leaf (no have child), delete node
+					return null;
+				}
+				if (node.left === null) { // if node have only left child, replace (and remove) current node with left child
+					node = node.right;
+					return node;
+				}
+				if (node.right === null) { // if node have only right child, replace (and remove) current node with right child
+					node = node.left;
+					return node;
+				}
+
+				let minNode = node.left;
+				while (minNode.right) {
+					minNode = minNode.right;
+				}
+				node.data = minNode.data;
+				node.left = removeNode(node.left, minNode.data);
+				return node;
+
+			}
+			// return node;
+		}
+	}
+
+
+	// ^------------------------ min ------------------------
+	min() {
+		if (this.r === null) return;
+		let node = this.r;
+		while (node.left) {
+			node = node.left;
+		}
+		return node.data;
+	}
+
+
+	// ^------------------------ max ------------------------
+	max() {
+		if (this.r === null) return;
+		let node = this.r;
+		while (node.right) {
+			node = node.right;
+		}
+		return node.data;
+	}
+
+
+	// ^------------------------ show ------------------------
+	show() { // test function
+		console.log(this.r);
+	}
 }
 
 module.exports = {
